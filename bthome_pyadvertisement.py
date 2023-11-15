3512f53d38fad7915dd1268d2f3f7aba72553ae3
bastian <nikofoxy@gmail.com>
2023-11-15 03:07:06 +0100

    fix: correct converting values to bytes

--- bthome_pyadvertisement.py
+++ bthome_pyadvertisement.py
@@ -1,9 +1,11 @@
 import json
+import struct
 
 
 class BTHomeAdvertisementData:
-    BT_HOME_FLAGS = [0x02, 0x01, 0x06]
-    BT_HOME_SERVICE_DATA_BYTE = 0x16
+    # Based on documentation: https://bthome.io/format/
+    BT_HOME_FLAGS = [0x02, 0x01, 0x06]  # per documentation Flags
+    BT_HOME_SERVICE_DATA_SPECIAL_BYTE = 0x16
     BT_HOME_UUID_BYTES = [0xD2, 0xFC]
     BT_HOME_DEVICE_INFORMATION_BYTE = 0x40
 
@@ -17,24 +19,35 @@ class BTHomeAdvertisementData:
         return adv_element
 
     @staticmethod
-    def value_to_little_endian(
-        measurement: float, measurement_factor: float, bytes_count: int
-    ):
-        # Calculate the integer value by dividing the temperature by the factor
-        integer_value = int(measurement / measurement_factor)
+    def value_to_bytes(value, data_type: str, factor: float, bytes_count: int):
+        # Determine the byte length based on the data type
+        if data_type in ["uint", "uint8"]:
+            struct_format = "B"
+            if bytes_count == 2:
+                struct_format = "H"
+        elif data_type == "uint16":
+            struct_format = "H"
+        elif data_type == "uint24":
+            struct_format = "I"
+        elif data_type == "sint16":
+            struct_format = "h"
+        else:
+            raise ValueError(f"Unsupported data type: {data_type}")
 
-        bytes_array = []
-        # Extract the lower and upper bytes for little-endian representation
-        for i in range(bytes_count):
-            lower_byte = integer_value & 0xFF
-            bytes_array.append(lower_byte)
-            integer_value = integer_value >> 8
+        # Add the byte order to the struct format (big endian)
+        struct_format = ">" + struct_format
 
-        # Reverse the order of the bytes
-        bytes_array.reverse()
+        byte_length = bytes_count
 
-        little_endian_bytes = bytes_array
-        return little_endian_bytes
+        # Convert float value to the integer representation based on the factor
+        integer_value = int(value / factor)
+
+        # Pack the integer value into bytes
+        packed_bytes = bytearray(struct.pack(struct_format, integer_value))
+
+        # Return the bytes
+        packed_bytes = packed_bytes[:byte_length]
+        return packed_bytes
 
     def get_sensor_data_bytes(self, sensor_data: dict):
         """
@@ -48,9 +61,10 @@ class BTHomeAdvertisementData:
         for key, value in sensor_data.items():
             # find measurement in bthome_sensor_data
             measurement = bthome_sensor_data[key]
-            measurement_bytes_value = self.value_to_little_endian(
-                measurement=value,
-                measurement_factor=measurement["factor"],
+            measurement_bytes_value = self.value_to_bytes(
+                value=value,
+                data_type=measurement["data_type"],
+                factor=measurement["factor"],
                 bytes_count=measurement["data_bytes_count"],
             )
             measurement_bytes.extend([int(measurement["service_data_byte"], 16)])
@@ -72,11 +86,11 @@ class BTHomeAdvertisementData:
         measurement_bytes = self.get_sensor_data_bytes(sensor_data=kwargs)
 
         service_data_bytes = (
-            [BTHomeAdvertisementData.BT_HOME_SERVICE_DATA_BYTE]
+            [BTHomeAdvertisementData.BT_HOME_SERVICE_DATA_SPECIAL_BYTE]
             + BTHomeAdvertisementData.BT_HOME_UUID_BYTES
             + [BTHomeAdvertisementData.BT_HOME_DEVICE_INFORMATION_BYTE]
-            + measurement_bytes
         )
+        service_data_bytes.extend(measurement_bytes)
 
         # We need to add 1 byte which is the length of the service data
         service_data_bytes_length = len(service_data_bytes)
